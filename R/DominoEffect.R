77d41a3ddf72bab3562071e7e4f0c53a1b865b63
Peter Blattmann <blattmann@imsb.biol.ethz.ch>
2019-02-22 14:53:42 +0100

    v1.1.3: change ensembl version from 73 to 75

--- R/DominoEffect.R
+++ R/DominoEffect.R
@@ -10,7 +10,7 @@ ratio.thr = 45, approach = "percentage", write_to_file = "NO"){
                                  approach)
 
     hotspot_mutations <- map_to_func_elem(results, write_to_file, 
-                                          ens_release = "73")
+                                          ens_release = "75")
     message ("   ***FINISHED***")
     return(hotspot_mutations)
 }
